404ac47cdd8800b477ee2d8973eda4ccc1141786
James Florez <jandrey2002@gmail.com>
2023-12-02 19:37:53 -0500

    Creación del proyecto

---
+++ src/main/java/org/Proyecto/SprintBoot_Reserva/SprintBootReservaApplication.java
@@ -0,0 +1,14 @@
+package org.Proyecto.SprintBoot_Reserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootReservaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SprintBootReservaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/Proyecto/SprintBoot_Reserva/SprintBootReservaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Proyecto.SprintBoot_Reserva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootReservaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
