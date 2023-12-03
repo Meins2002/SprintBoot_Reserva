774bd43a5a2ee2ff838d77fae8a3e30786748eec
James Florez <jandrey2002@gmail.com>
2023-12-03 17:12:08 -0500

    Implementación de metodo

--- src/main/java/org/Proyecto/SprintBoot_Reserva/controller/health/HealthController.java
+++ src/main/java/org/Proyecto/SprintBoot_Reserva/controller/health/HealthController.java
@@ -1,16 +1,17 @@
 package org.Proyecto.SprintBoot_Reserva.controller.health;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-   @RequestMapping("/health")
+   @RequestMapping("/health/{name}")
 
-    public String checkAPI(){
+    public String checkAPI(@PathVariable (value = "name")String name){
 
-       return "<h1>The API is working ok!</h1>";
+       return String.format("<h1>The API is working ok!</h1>");
 
    }
 }
